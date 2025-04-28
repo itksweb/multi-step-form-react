fc37ef735120ed5c29d9be93657897fa2516e1c4
itksweb <itksweb@gmail.com>
2025-04-28 15:57:47 +0100

    progressing

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@
 
 const Sidebar = () => {
   return (
-    <div className="col-span-1 min-w-80 h-full bg-no-repeat bg-contain bg-[url(./assets/images/bg-sidebar-desktop.svg)]">Sidebar</div>
+    <div className="col-span-1 min-w-80 h-full bg-no-repeat bg-contain p-4 bg-[url(./assets/images/bg-sidebar-desktop.svg)]">Sidebar</div>
   )
 }
 
