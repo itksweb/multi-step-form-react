3ced60f767865ffda12853a91fb03de0125a66b4
itksweb <itksweb@gmail.com>
2025-04-28 16:07:43 +0100

    step button added

--- src/App.jsx
+++ src/App.jsx
@@ -16,17 +16,33 @@ const App = () => {
   const changePlan = () =>{
 
   }
+  const nextStep =(e)=>{
+    e.preventDefault()
+    if (step < 4) {
+      setStep(prev => prev + 1)
+    }
+  }
+  const prevStep =(e)=>{
+    e.preventDefault()
+    if (step > 1) {
+      setStep(prev => prev - 1)
+    }
+  }
 
   return (
-    <div className="grid grid-flow-col grid-cols-3 bg-white p-4 gap-8 min-h-[80vh] rounded-2xl ">
+    <div className="grid grid-flow-col grid-cols-3 text-black bg-white p-4 gap-8 min-h-[80vh] rounded-2xl ">
       <Sidebar />
-      <main className="col-span-2 bg-purple-950 rounded-2xl p-4">
+      <main className="col-span-2 rounded-2xl p-4 flex flex-col justify-between">
         <div className="content">
           {step === 1 && <PersonalInfo setUserInfo={setUserInfo} />}
           {step === 2 && <Plan setPlan = {setPlan} isMonthly={isMonthly} />}
           {step === 3 && <AddOns setAddons={setAddons} isMonthly={isMonthly} />}
           {step === 4 && <Summary changePlan={changePlan} />}
-      </div>
+        </div>
+        <div className="btns flex justify-between">
+          <button className="btn" onClick={prevStep}>Back</button>
+          <button className="btn" onClick={nextStep} >Next</button>
+        </div>
       </main>
     </div>
   )
