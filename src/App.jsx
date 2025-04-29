061d917c127f27ed3886dbb0f0f9888266d2ce66
itksweb <66902194+itksweb@users.noreply.github.com>
2025-04-29 12:44:38 +0100

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AddOns from "./components/AddOns"
 import Summary from "./components/Summary"
 
 
+
 const App = () => {
   const [userInfo, setUserInfo  ] = useState({name:"", phone:"", email:""});
   const [plan, setPlan] = useState({name:"", cost:""});
@@ -31,13 +32,13 @@ const App = () => {
 
   return (
     <div className="grid grid-flow-col grid-cols-3 text-black bg-white p-4 gap-8 min-h-[80vh] rounded-2xl ">
-      <Sidebar />
+      <Sidebar step={step} />
       <main className="col-span-2 rounded-2xl p-4 flex flex-col justify-between">
         <div className="content">
           {step === 1 && <PersonalInfo setUserInfo={setUserInfo} />}
           {step === 2 && <Plan setPlan = {setPlan} isMonthly={isMonthly} />}
           {step === 3 && <AddOns setAddons={setAddons} isMonthly={isMonthly} />}
-          {step === 4 && <Summary changePlan={changePlan} />}
+          {step === 4 && <Summary isMonthly={isMonthly}  changePlan={changePlan} />}
         </div>
         <div className="btns flex justify-between">
           <button className="btn" onClick={prevStep}>Back</button>
