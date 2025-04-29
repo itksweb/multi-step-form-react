4c4fa3c29b6d45964f9fd23ee2cd2dbdc485058f
itksweb <66902194+itksweb@users.noreply.github.com>
2025-04-29 12:43:42 +0100

    Sibebar updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
 import bgSidebar from "../images/bg-sidebar-desktop.svg"
 import mobiBgSidebar from "../images/bg-sidebar-mobile.svg"
 
-const Sidebar = () => {
+const Sidebar = ({step}) => {
   return (
-    <div className={`col-span-1 min-w-80 h-full bg-no-repeat bg-contain p-4 bg-[url(${bgSidebar})]`}>Sidebar</div>
+    <div className={`col-span-1 min-w-80 h-full bg-no-repeat bg-contain p-4 bg-[url(${bgSidebar})]`}>Step {step}</div>
   )
 }
 
